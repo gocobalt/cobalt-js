9dd5dcd23658016cf7d83497c7c4c08ac372d3b1
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-01-30 12:19:37 +0530

    refactor: reorder method order
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- cobalt.js
+++ cobalt.js
@@ -30,6 +30,138 @@ class Cobalt {
     };
 
     /**
+     * Returns the auth URL that users can use to authenticate themselves to the
+     * specified application.
+     * @private
+     * @param {String} application The application type.
+     * @returns {Promise<String>} The auth URL where users can authenticate themselves.
+     */
+    async getOAuthUrl(application) {
+        const res = await fetch(`${this.baseUrl}/api/v1/${application}/integrate`, {
+            headers: {
+                authorization: `Bearer ${this.token}`,
+            },
+        });
+
+        if (res.status >= 400 && res.status < 600) {
+            throw new Error(res.statusText);
+        }
+
+        const data = await res.json();
+        return data?.auth_url;
+    }
+
+    /**
+     * Handle OAuth for the specified application.
+     * @param {String} application The application type.
+     * @returns {Promise<Boolean>} Whether the user authenticated.
+     */
+    async oauth(application) {
+        return new Promise((resolve, reject) => {
+            this.getOAuthUrl(application)
+            .then(oauthUrl => {
+                const connectWindow = window.open(oauthUrl);
+
+                // keep checking connection status
+                const interval = setInterval(() => {
+                    this.getAppAuthStatus(application)
+                    .then(connected => {
+                        if (connected === true) {
+                            // close auth window
+                            connectWindow?.close();
+                            // clear interval
+                            clearInterval(interval);
+                            // resovle status
+                            resolve(true);
+                        } else {
+                            // user closed oauth window without authenticating
+                            if (connectWindow?.closed) {
+                                // clear interval
+                                clearInterval(interval);
+                                // resolve status
+                                resolve(false);
+                            }
+                        }
+                    })
+                    .catch(e => {
+                        console.error(e);
+                        clearInterval(interval);
+                        reject(e);
+                    });
+                }, 3e3);
+            })
+            .catch(reject);
+        });
+    }
+
+    /**
+     * Save the auth data that user provides to authenticate themselves to the
+     * specified application.
+     * @param {String} application The application type.
+     * @param {Object.<string, string | number | boolean>} payload The key value pairs of auth data.
+     * @param {String} applicationId The application ID in case of custom applications.
+     * @returns {Promise<unknown>}
+     */
+    async setAppAuthData(application, payload, applicationId) {
+        const res = await fetch(applicationId ? `${this.baseUrl}/api/v1/${application}/${applicationId}/save` : `${this.baseUrl}/api/v1/${application}/save`, {
+            method: "POST",
+            headers: {
+                authorization: `Bearer ${this.token}`,
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                ...payload,
+            }),
+        });
+
+        if (res.status >= 400 && res.status < 600) {
+            throw new Error(res.statusText);
+        }
+
+        const data = await res.json();
+        return data;
+    }
+
+    /**
+     * Returns the auth status of the user for the specified application.
+     * @param {String} application The application type.
+     * @returns {Promise<Boolean>} The auth status of the user.
+     */
+    async getAppAuthStatus(application) {
+        const res = await fetch(`${this.baseUrl}/api/v1/linked-acc/integration/auth?integration_type=${application}`, {
+            headers: {
+                authorization: `Bearer ${this.token}`,
+            },
+        });
+
+        if (res.status >= 400 && res.status < 600) {
+            throw new Error(res.statusText);
+        }
+
+        const data = await res.json();
+        return !!data?.status;
+    }
+
+    /**
+     * Unauthorize the specified application and remove any associated data from Cobalt.
+     * @param {String} application The application type.
+     * @param {String} applicationId The application ID in case of custom applications.
+     * @returns {Promise<void>}
+     */
+    async removeAppAuth(application, applicationId) {
+        const res = await fetch(`${this.baseUrl}/api/v1/linked-acc/integration/${application}?app_id=${applicationId}`, {
+            method: "DELETE",
+            headers: {
+                authorization: `Bearer ${this.token}`,
+            },
+        });
+
+        if (res.status >= 400 && res.status < 600) {
+            throw new Error(res.statusText);
+        }
+    }
+
+    /**
      * @typedef {object} AppConfig The configuration data for an application.
      * @property {DataSlot[]} application_data_slots Array of application data slots.
      * @property {Template[]} templates Array of workflow templates.
@@ -154,138 +286,6 @@ class Cobalt {
         }
 
         return await res.json();
-    }
-
-    /**
-     * Returns the auth status of the user for the specified application.
-     * @param {String} application The application type.
-     * @returns {Promise<Boolean>} The auth status of the user.
-     */
-    async getAppAuthStatus(application) {
-        const res = await fetch(`${this.baseUrl}/api/v1/linked-acc/integration/auth?integration_type=${application}`, {
-            headers: {
-                authorization: `Bearer ${this.token}`,
-            },
-        });
-
-        if (res.status >= 400 && res.status < 600) {
-            throw new Error(res.statusText);
-        }
-
-        const data = await res.json();
-        return !!data?.status;
-    }
-
-    /**
-     * Returns the auth URL that users can use to authenticate themselves to the
-     * specified application.
-     * @private
-     * @param {String} application The application type.
-     * @returns {Promise<String>} The auth URL where users can authenticate themselves.
-     */
-    async getOAuthUrl(application) {
-        const res = await fetch(`${this.baseUrl}/api/v1/${application}/integrate`, {
-            headers: {
-                authorization: `Bearer ${this.token}`,
-            },
-        });
-
-        if (res.status >= 400 && res.status < 600) {
-            throw new Error(res.statusText);
-        }
-
-        const data = await res.json();
-        return data?.auth_url;
-    }
-
-    /**
-     * Handle OAuth for the specified application.
-     * @param {String} application The application type.
-     * @returns {Promise<Boolean>} Whether the user authenticated.
-     */
-    async oauth(application) {
-        return new Promise((resolve, reject) => {
-            this.getOAuthUrl(application)
-            .then(oauthUrl => {
-                const connectWindow = window.open(oauthUrl);
-
-                // keep checking connection status
-                const interval = setInterval(() => {
-                    this.getAppAuthStatus(application)
-                    .then(connected => {
-                        if (connected === true) {
-                            // close auth window
-                            connectWindow?.close();
-                            // clear interval
-                            clearInterval(interval);
-                            // resovle status
-                            resolve(true);
-                        } else {
-                            // user closed oauth window without authenticating
-                            if (connectWindow?.closed) {
-                                // clear interval
-                                clearInterval(interval);
-                                // resolve status
-                                resolve(false);
-                            }
-                        }
-                    })
-                    .catch(e => {
-                        console.error(e);
-                        clearInterval(interval);
-                        reject(e);
-                    });
-                }, 3e3);
-            })
-            .catch(reject);
-        });
-    }
-
-    /**
-     * Save the auth data that user provides to authenticate themselves to the
-     * specified application.
-     * @param {String} application The application type.
-     * @param {Object.<string, string | number | boolean>} payload The key value pairs of auth data.
-     * @param {String} applicationId The application ID in case of custom applications.
-     * @returns {Promise<unknown>}
-     */
-    async setAppAuthData(application, payload, applicationId) {
-        const res = await fetch(applicationId ? `${this.baseUrl}/api/v1/${application}/${applicationId}/save` : `${this.baseUrl}/api/v1/${application}/save`, {
-            method: "POST",
-            headers: {
-                authorization: `Bearer ${this.token}`,
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                ...payload,
-            }),
-        });
-
-        if (res.status >= 400 && res.status < 600) {
-            throw new Error(res.statusText);
-        }
-
-        const data = await res.json();
-        return data;
-    }
-
-    /**
-     * Unauthorize the specified application and remove any associated data from Cobalt.
-     * @param {String} application The application type.
-     * @param {String} applicationId The application ID in case of custom applications.
-     * @returns {Promise<void>}
-     */
-    async removeAppAuth(application, applicationId) {
-        const res = await fetch(`${this.baseUrl}/api/v1/linked-acc/integration/${application}?app_id=${applicationId}`, {
-            method: "DELETE",
-            headers: {
-                authorization: `Bearer ${this.token}`,
-            },
-        });
-
-        if (res.status >= 400 && res.status < 600) {
-            throw new Error(res.statusText);
-        }
     }
 }
 
