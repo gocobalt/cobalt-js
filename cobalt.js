61246392db43f9fbb4a3b75fce38e339fd82bb0e
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-05-04 15:24:06 +0530

    feat: make slug optional
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- cobalt.js
+++ cobalt.js
@@ -34,12 +34,13 @@ class Cobalt {
 
     /**
      * Returns the application details for the specified application, provided
-     * the application is enabled in Cobalt.
-     * @param {String} slug The application slug.
+     * the application is enabled in Cobalt. If no application is specified,
+     * it returns all the enabled applications.
+     * @param {String} [slug] The application slug.
      * @returns {Promise<Application>} The application details.
      */
     async getApp(slug) {
-        const res = await fetch(`${this.baseUrl}/api/v1/linked-acc/application/${slug}`, {
+        const res = await fetch(`${this.baseUrl}/api/v1/linked-acc/application` + (slug ? `/${slug}` : ""), {
             headers: {
                 authorization: `Bearer ${this.token}`,
             },
