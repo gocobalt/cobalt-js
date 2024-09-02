3103ceab4439c2d26d589002fe874e8ef0180ef3
TRACTION <19631364+iamtraction@users.noreply.github.com>
2024-09-02 16:15:23 +0530

    refactor: getFieldOptions method
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- cobalt.ts
+++ cobalt.ts
@@ -557,14 +557,14 @@ class Cobalt {
     }
 
     /**
-     * Returns the options for the specified rule field.
-     * @param {String} lhs The lhs value of the rule field.
+     * Returns the options for the specified field.
+     * @param {String} label The selected value of the label field.
      * @param {String} slug The application slug.
      * @param {String} fieldId The unique ID of the field.
      * @param {String} [workflowId] The unique ID of the workflow, if this is a workflow field.
      * @returns {Promise<RuleOptions>} The specified rule field's options.
      */
-    async getRuleFieldOptions(lhs: string, slug: string, fieldId: string, workflowId?: string): Promise<RuleOptions> {
+    async getFieldOptions(label: string, slug: string, fieldId: string, workflowId?: string): Promise<RuleOptions> {
         const res = await fetch(`${this.baseUrl}/api/v2/public/config/rule-engine/${fieldId}${workflowId ? `?workflow_id=${workflowId}` : ""}`, {
             method: "POST",
             headers: {
@@ -573,7 +573,7 @@ class Cobalt {
                 slug,
             },
             body: JSON.stringify({
-                rule_column: { lhs },
+                rule_column: { lhs: label },
             }),
         });
 
